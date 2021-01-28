9b2cd49ae774d215bdb709e180f223eb55edd7a7
tommydemarco <tommaso.demarco95@gmail.com>
2021-01-28 23:20:51 +0100

    Initial commit, template files added

---
+++ pages/login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Layouts/Navbar';
+import Footer from '../components/Layouts/Footer';
+import Banner from '../components/Login/Banner';
+import LoginBody from '../components/Login/LoginBody';
+
+class Login extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar />
+                <Banner />
+                <LoginBody />
+                <Footer />
+            </React.Fragment>
+        );
+    }
+}
+
+export default Login;
---
+++ pages/shop-one.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Layouts/Navbar';
+import Footer from '../components/Layouts/Footer';
+import Banner from '../components/Shop/Banner';
+import ShopVOne from '../components/Shop/ShopVOne';
+
+class ShopOne extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar />
+                <Banner />
+                <ShopVOne />
+                <Footer />
+            </React.Fragment>
+        );
+    }
+}
+
+export default ShopOne;
