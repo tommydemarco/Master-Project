37d995128773b6955f9c13d7f1dfb858afd751a9
Tommaso De Marco <tommaso.de-marco@odt.net>
2021-04-02 01:04:43 +0200

    Add pages for new post and new blog

---
+++ pages/admin/add-post.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+import { useContext } from "react";
+import { bannerContext } from "../../context";
+import { useBannerUpdate } from "../../utils/hooks";
+
+import Banner from "../../components/Banner"
+
+const addPost = props => {
+    const { dispatch } = useContext(bannerContext);
+    useBannerUpdate(dispatch, true);
+
+    return (
+        <>
+           <Banner title="Add a new post" subtitle="Use this form to add a new post" />
+        </>
+    )
+}
+
+addPost.propTypes = {
+
+}
+
+export default addPost
---
+++ pages/admin/add-product.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+
+import Banner from "../../components/Banner"
+
+const addProduct = props => {
+    return (
+        <>
+           <Banner title="Add a product" subtitle="Use this form to add a prduct to the product list" />
+        </>
+    )
+}
+
+addProduct.propTypes = {
+
+}
+
+export default addProduct
