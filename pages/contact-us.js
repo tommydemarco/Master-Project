d456cdf2deec49423b7e2277285ed29fb3cd09d1
Tommaso De Marco <tommaso.de-marco@odt.net>
2021-04-03 20:50:15 +0200

    Create contactCards compoent

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -3,8 +3,8 @@ import { bannerContext } from "../context";
 import { useBannerUpdate } from "../utils/hooks";
 
 import Banner from "../components/Banner";
-import Content from "../components/ContactUs/Content";
-import Form from "../components/ContactForm";
+import ContactCards from "../components/ContactCards";
+import ContactForm from "../components/ContactForm";
 
 const ContactUsPage = () => {
     const { dispatch } = useContext(bannerContext);
@@ -16,8 +16,8 @@ const ContactUsPage = () => {
                 title="Get in touch with us"
                 subtitle="Anything On your Mind. We’ll Be Glad To Assist You!"
             />
-            <Form />
-            <Content />
+            <ContactForm />
+            <ContactCards />
         </>
     );
 };
