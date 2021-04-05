f7f1c6cc8401b2700003bcba64f73fb6ae1f052c
Tommaso De Marco <tommaso.de-marco@odt.net>
2021-04-05 20:15:04 +0200

    Add cypress test for form validation

--- cypress/integration/contact-us/formErrors.spec.js
+++ cypress/integration/contact-us/formErrors.spec.js
@@ -2,10 +2,14 @@
 
 describe("Simulate form validation errors", () => {
     it("shows an error if name input is deleted", () => {
-        cy.visit("http://localhost:3000");
-        cy.get("input[name='name']");
-        cy.type("Alex");
-        cy.type("{del}{del}{del}{del}");
+        cy.visit("http://localhost:3000/contact-us");
+        cy.get("input[name='name']").type("Alex", { force: true });
+        cy.get("input[name='name']").type(
+            "{backspace}{backspace}{backspace}{backspace}",
+            {
+                force: true,
+            }
+        );
         cy.wait(500);
         cy.get("input[name='name']").siblings().should("have.length", 1);
     });
